af4f2d2c3749ca967f263e99e95d03cecc9506f0
eiiti <eiitiw@gmeil.com>
2021-06-12 14:34:29 +0900

    一時保存ファイルを入れる関数を追加
    サンプルプログラムやドキュメントを辿って組み合わせて作られている。

--- mylinebot.py
+++ mylinebot.py
@@ -8,7 +8,7 @@ from linebot import (
     LineBotApi, WebhookHandler
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage, ImageMessage,
 )
 
 handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
@@ -38,3 +38,18 @@ def handle_text_message(event):
         TextSendMessage(text='Hello World'))
 #        TextSendMessage(text=input_text))
 
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image_message(event):
+    # ユーザーから送られてきた画像を一時ファイルとして保存
+    message_content = line_bot_api.get_message_content(event.message.id)
+    file_path = "/tmp/sent-image.jpg"
+    with open(file_path, 'wb') as fd:
+        for chunk in message_content.iter_content():
+            fd.write(chunk)
+
+    # Rekognition で感情分析する
+
+    # 返答を送信する
+
+    # file_path の画像を削除
+    os.remove(file_path)
